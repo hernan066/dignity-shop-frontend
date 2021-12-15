d33e21604ac48311c17d45dd8c281ffbf787191f
hernan066 <hernan0066@gmail.com>
2021-12-15 02:17:54 -0300

    HashRouter

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 import {
-    BrowserRouter,
+    HashRouter,
     Routes,
     Route,
   } from 'react-router-dom';
@@ -27,7 +27,7 @@ const AppRouter = () => {
   
   
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/producto/:id" element={<Detalle />} />
@@ -59,7 +59,7 @@ const AppRouter = () => {
                         
 
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 };
 
